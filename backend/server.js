543d59470fbbd9a3cb41ffa4b89882851abcfd2d
darrenpatricknelson <nelson.darrenp10.dnp@gmail.com>
2022-05-13 11:51:08 +0200

    FEAT: prepping for deployment (Heroku)

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,17 @@ app.get("/", (req, res) => {
 app.use("/api", routes);
 
 // port
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log("Listening on port:", PORT);
 });
+
+// call React build assest
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
+
+app.use(express.static(path.join(__dirname, "frontend/build")));
